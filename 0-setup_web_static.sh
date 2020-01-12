d1deef4124e8a8df76de03e1b40370e5c21f7d0e
guxal <jonathanacp93@gmail.com>
2020-01-12 05:46:33 +0000

    add /

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu. /data
 sudo sh -c "echo '<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' > /data/web_static/releases/test/index.html"
-sudo sed  -ie 's|^\t}|\t}\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}|' /etc/nginx/sites-available/default
+sudo sed  -ie 's|^\t}|\t}\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}|' /etc/nginx/sites-available/default
 sudo service nginx restart
