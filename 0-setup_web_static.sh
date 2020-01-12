ddbedac603d747c1159542ebb986e4047d06b226
guxal <jonathanacp93@gmail.com>
2020-01-12 05:45:01 +0000

    add two line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu. /data
+sudo chown -hR ubuntu. /data
 sudo sh -c "echo '<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' > /data/web_static/releases/test/index.html"
 sudo sed  -ie 's|^\t}|\t}\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}|' /etc/nginx/sites-available/default
 sudo service nginx restart
