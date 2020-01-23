76c09f0a5631617508751b79a1acbd0b33e358a0
JonnyCash <jonathanacp93@gmail.com>
2020-01-23 00:04:40 -0500

    hotfix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,3 +30,7 @@ def python(text="is cool"):
 @app.route('/number/<int:n>')
 def number(n):
     return ('%d is a number' % n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
